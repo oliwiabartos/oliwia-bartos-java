8ec1b88cca91d9fc812460e7b7d63419bff033ff
Oliwia Bartos <oliwiabartos@gmail.com>
2018-09-19 22:11:39 +0200

    first commit

---
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -0,0 +1,7 @@
+package com.kodilla.kodillacourse;
+
+public class Calculator {
+    public double add(double a, double b){
+        return a+b;
+    }
+}
---
+++ src/main/java/com/kodilla/kodillacourse/Runner.java
@@ -0,0 +1,8 @@
+package com.kodilla.kodillacourse;
+
+public class Runner {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(10,5));
+    }
+}
