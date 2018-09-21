9b83f7e82ef88abf4a68621343aac98dfa07be0f
Oliwia Bartos <oliwiabartos@gmail.com>
2018-09-21 11:33:50 +0200

    xxxxxxx

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,4 @@
 package com.kodilla.testing;
 
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.print("Modul 6. Wprowadzenie do oprogramowania");
-    }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Runner.java
@@ -0,0 +1,10 @@
+package com.kodilla.testing.calculator;
+
+    public class Runner {
+        public static void main(String[] args) {
+            Calculator calc = new Calculator();
+            System.out.println(calc.add(555, 425));
+            System.out.println(calc.subtract(85123, 7452));
+        }
+    }
+
