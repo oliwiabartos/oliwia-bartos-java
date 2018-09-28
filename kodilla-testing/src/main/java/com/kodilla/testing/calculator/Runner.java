50419d9b42d2aa889ed4bacff45a0b4eba3d0729
Oliwia Bartos <oliwiabartos@gmail.com>
2018-09-28 22:37:11 +0200

    My second respository

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Runner.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Runner.java
@@ -1,10 +1,23 @@
 package com.kodilla.testing.calculator;
 
-    public class Runner {
+public class Runner {
         public static void main(String[] args) {
             Calculator calc = new Calculator();
-            System.out.println(calc.add(555, 425));
-            System.out.println(calc.subtract(85123, 7452));
+
+            int res1 = calc.add(555, 445);
+            int res2 = calc.subtract(1000, 100);
+           System.out.println(res1);
+            System.out.println(res2);
+
+            if (res1 == 1000)
+                if (res2 == 900) {
+                    System.out.println("Working");
+                }else{
+                    System.out.println("Doesn't working");
+
+
+                }
         }
-    }
+        }
+
 
